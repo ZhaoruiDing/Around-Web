2d0918c244f8469f0e84dad7dd9e9c20938fdb06
ZhaoruiDing <zhaorui1225@gmail.com>
2018-07-29 16:48:38 -0500

    Login UI

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Register} from './Register'
+import {Login} from "./Login"
 export class Main extends React.Component{
   render(){
     return(
       <div>
-        <Register/>
+        <Login/>
       </div>
     );
   }
