5c4d9420275d883c932e132bd7f546c9ca8e0780
Zhaorui Ding <zhaorui1225@gmail.com>
2018-08-06 05:41:38 +0800

    localstorage init

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,13 +10,20 @@ export class Main extends React.Component{
     return this.props.isLoggedIn ? <Redirect to="/home"/> : <Login handleLogin={this.props.handleLogin}/>;
   }
 
+  getHome = () => {
+    return this.props.isLoggedIn ? <Home/> : <Redirect to="/login"/>;
+  }
+
+  getRoot = () => {
+    return <Redirect to="/login"/>;
+  }
   render(){
     return(
       <div>
 
         <Switch>
-          <Route exact path="/" render={this.getLogin}/>
-          <Route path="/home" component={Home}/>
+          <Route exact path="/" render={this.getRoot}/>
+          <Route path="/home" render={this.getHome}/>
           <Route path="/login" render={this.getLogin}/>
           <Route path="/register" component={Register}/>
           <Route render={this.getLogin}/>
