d0e9ab55336f26778ff8075868523ca13ebd6970
ZhaoruiDing <zhaorui1225@gmail.com>
2018-08-06 18:22:59 +0800

    load geolocation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,33 @@
 import React from 'react';
-
+import {GEO_LOCATION} from "../constants"
 
 export class Home extends React.Component {
+  componentDidMount(){
+    this.getGeolocation();
+  }
+
+  getGeolocation = ()=>{
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        this.onSuccessLoadGeoLocation,
+        this.onFailedLoadGeoLocation,
+        GEO_LOCATION,
+      );
+    } else {
+      /* geolocation IS NOT available */
+    }
+  }
+
+
+  onSuccessLoadGeoLocation= (position) => {
+    console.log(position);
+  }
+
+  onFailedLoadGeoLocation = (error) => {
+    console.log(error);
+
+  }
+
   render() {
     return (
       <div>
